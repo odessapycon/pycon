4018332f7515fb5c01798f78619ab9f5a4e64c81
Artem <as@attractgroup.com>
2019-01-13 21:03:08 +0200

    info added

--- resources/lang/js/en/main-speakers-en.js
+++ resources/lang/js/en/main-speakers-en.js
@@ -1,10 +1,39 @@
 export const MainSpeakersEn = [
+  // {
+  //   image : '../public/img/cont/speakers/caceres.jpg',
+  //   name : 'Anton Caceres',
+  //   position : 'CEO',
+  //   company : 'TECH-5',
+  //   place : 'Munich, Germany',
+  //   rept : [
+  //     {
+  //       title : '',
+  //       description : ''
+  //     }
+  //   ],
+  //   aboutSpeaker : '',
+  //   socialsRendered : '',
+  //   socials : [
+  //     {
+  //       link : 'http://github.com/ma3str0',
+  //       fatype : 'github'
+  //     },
+  //     {
+  //       link : 'https://twitter.com/anton_caceres',
+  //       fatype : 'twitter'
+  //     },
+  //     {
+  //       link : 'https://caceres.me',
+  //       fatype : 'link'
+  //     },
+  //   ]
+  // },
   {
-    image : '../public/img/cont/speakers/caceres.jpg',
-    name : 'Anton Caceres',
-    position : 'CEO',
-    company : 'TECH-5',
-    place : 'Munich, Germany',
+    image : '../public/img/cont/speakers/vsevolod_solovyov.jpg',
+    name : 'Vsevolod Solovyov',
+    position : 'Co-founder and CTO',
+    company : 'Prophy Science',
+    place : 'Kiev, Ukraine',
     rept : [
       {
         title : '',
@@ -15,17 +44,17 @@ export const MainSpeakersEn = [
     socialsRendered : '',
     socials : [
       {
-        link : 'http://github.com/ma3str0',
-        fatype : 'github'
-      },
-      {
-        link : 'https://twitter.com/anton_caceres',
+        link : 'https://twitter.com/murkt',
         fatype : 'twitter'
       },
       {
-        link : 'https://caceres.me',
-        fatype : 'link'
+        link : 'https://www.linkedin.com/in/vsolovyov/',
+        fatype : 'linkedin'
       },
+      {
+        link : 'https://www.facebook.com/vsevolod.solovyov',
+        fatype : 'facebook'
+      }
     ]
   },
 ];
--- resources/lang/js/ru/main-speakers-ru.js
+++ resources/lang/js/ru/main-speakers-ru.js
@@ -1,10 +1,39 @@
 export const MainSpeakersRu = [
+  // {
+  //   image : 'public/img/cont/speakers/caceres.jpg',
+  //   name : 'Anton Caceres',
+  //   position : 'CEO',
+  //   company : 'TECH-5',
+  //   place : 'Мюнхен, Германия',
+  //   rept : [
+  //     {
+  //       title : '',
+  //       description : ''
+  //     }
+  //   ],
+  //   aboutSpeaker : '',
+  //   socialsRendered : '',
+  //   socials : [
+  //     {
+  //       link : 'http://github.com/ma3str0',
+  //       fatype : 'github'
+  //     },
+  //     {
+  //       link : 'https://twitter.com/anton_caceres',
+  //       fatype : 'twitter'
+  //     },
+  //     {
+  //       link : 'https://caceres.me',
+  //       fatype : 'link'
+  //     },
+  //   ]
+  // },
   {
-    image : 'public/img/cont/speakers/caceres.jpg',
-    name : 'Anton Caceres',
-    position : 'CEO',
-    company : 'TECH-5',
-    place : 'Мюнхен, Германия',
+    image : 'public/img/cont/speakers/vsevolod_solovyov.jpg',
+    name : 'Всеволод Соловьёв',
+    position : 'Co-founder and CTO',
+    company : 'Prophy Science',
+    place : 'Киев, Украина',
     rept : [
       {
         title : '',
@@ -15,16 +44,16 @@ export const MainSpeakersRu = [
     socialsRendered : '',
     socials : [
       {
-        link : 'http://github.com/ma3str0',
-        fatype : 'github'
-      },
-      {
-        link : 'https://twitter.com/anton_caceres',
+        link : 'https://twitter.com/murkt',
         fatype : 'twitter'
       },
       {
-        link : 'https://caceres.me',
-        fatype : 'link'
+        link : 'https://www.linkedin.com/in/vsolovyov/',
+        fatype : 'linkedin'
+      },
+      {
+        link : 'https://www.facebook.com/vsevolod.solovyov',
+        fatype : 'facebook'
       },
     ]
   },
