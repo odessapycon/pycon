eb6ade178955ed54fcdcf71a89e16ae039af8869
wayfaerer <bodomhead@gmail.com>
2020-01-16 13:34:35 +0200

    schedule and speaker

--- resources/lang/js/ru/main-schedule-2020-ru.js
+++ resources/lang/js/ru/main-schedule-2020-ru.js
@@ -1,13 +1,244 @@
 export const MainSchedule2020Ru = [
-  /*{
-    time : {
-      start : '9:00',
-      end : '10:00'
+    {
+        time : {
+            start : '08.30',
+            end : '09.30'
+        },
+        icon : 'user', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Регистрация'}
+        ]
     },
-    icon : 'user', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
-    scheduleRendered : '',
-    talks : [
-      {author : '', title : 'Registration'}
-    ]
-  }*/
+    {
+        time : {
+            start : '09.30',
+            end : '10:10'
+        },
+        icon : 'file-text', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Доклад 1'}
+        ]
+    },
+    {
+        time : {
+            start : '10:10',
+            end : '10:25'
+        },
+        icon : 'clock-o', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Перерыв'}
+        ]
+    },
+    {
+        time : {
+            start : '10:25',
+            end : '11:05'
+        },
+        icon : 'file-text', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Доклад 2'}
+        ]
+    },
+    {
+        time : {
+            start : '11:05',
+            end : '11:20'
+        },
+        icon : 'clock-o', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Перерыв'}
+        ]
+    },
+    {
+        time : {
+            start : '11.20',
+            end : '12.00'
+        },
+        icon : 'file-text', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Доклад 3'}
+        ]
+    },
+    {
+        time : {
+            start : '12:00',
+            end : '12:15'
+        },
+        icon : 'clock-o', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Перерыв'}
+        ]
+    },
+    {
+        time : {
+            start : '12:15',
+            end : '12:55'
+        },
+        icon : 'file-text', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Доклад 4'}
+        ]
+    },
+    {
+        time : {
+            start : '12:55',
+            end : '14:00'
+        },
+        icon : 'cutlery', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Обед'}
+        ]
+    },
+    {
+        time : {
+            start : '14:00',
+            end : '14:40'
+        },
+        icon : 'file-text', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Доклад 5'}
+        ]
+    },
+    {
+        time : {
+            start : '14:40',
+            end : '14:55'
+        },
+        icon : 'clock-o', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Перерыв'}
+        ]
+    },
+    {
+        time : {
+            start : '14:55',
+            end : '15:35'
+        },
+        icon : 'file-text', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Доклад 6'}
+        ]
+    },
+    {
+        time : {
+            start : '15:35',
+            end : '15:50'
+        },
+        icon : 'clock-o', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Перерыв'}
+        ]
+    },
+    {
+        time : {
+            start : '15:50',
+            end : '16:30'
+        },
+        icon : 'file-text', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Доклад 7'}
+        ]
+    },
+    {
+        time : {
+            start : '16:30',
+            end : '16:45'
+        },
+        icon : 'clock-o', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Перерыв'}
+        ]
+    },
+    {
+        time : {
+            start : '16:45',
+            end : '17:25'
+        },
+        icon : 'file-text', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Доклад 8'}
+        ]
+    },
+    {
+        time : {
+            start : '17:25',
+            end : '17:40'
+        },
+        icon : 'clock-o', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Перерыв'}
+        ]
+    },
+    {
+        time : {
+            start : '17:40',
+            end : '18:20'
+        },
+        icon : 'file-text', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Доклад 9'}
+        ]
+    },
+    {
+        time : {
+            start : '18:20',
+            end : '18:35'
+        },
+        icon : 'clock-o', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Перерыв'}
+        ]
+    },
+    {
+        time : {
+            start : '18:35',
+            end : '19:15'
+        },
+        icon : 'file-text', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Доклад 10'}
+        ]
+    },
+    {
+        time : {
+            start : '19:15',
+            end : '19:30'
+        },
+        icon : 'flag', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Закрытие конференции'}
+        ]
+    },
+    {
+        time : {
+            start : '19:30',
+            end : '22:00'
+        },
+        icon : 'glass', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'After-party'}
+        ]
+    }
 ];
--- resources/lang/js/ua/main-schedule-2020-ua.js
+++ resources/lang/js/ua/main-schedule-2020-ua.js
@@ -1,13 +1,244 @@
 export const MainSchedule2020Ua = [
-  /*{
-    time : {
-      start : '9:00',
-      end : '10:00'
+    {
+        time : {
+            start : '08.30',
+            end : '09.30'
+        },
+        icon : 'user', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Реєстрація'}
+        ]
     },
-    icon : 'user', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
-    scheduleRendered : '',
-    talks : [
-      {author : '', title : 'Registration'}
-    ]
-  }*/
+    {
+        time : {
+            start : '09.30',
+            end : '10:10'
+        },
+        icon : 'file-text', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Доповідь 1'}
+        ]
+    },
+    {
+        time : {
+            start : '10:10',
+            end : '10:25'
+        },
+        icon : 'clock-o', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Перерва'}
+        ]
+    },
+    {
+        time : {
+            start : '10:25',
+            end : '11:05'
+        },
+        icon : 'file-text', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Доповідь 2'}
+        ]
+    },
+    {
+        time : {
+            start : '11:05',
+            end : '11:20'
+        },
+        icon : 'clock-o', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Перерва'}
+        ]
+    },
+    {
+        time : {
+            start : '11.20',
+            end : '12.00'
+        },
+        icon : 'file-text', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Доповідь 3'}
+        ]
+    },
+    {
+        time : {
+            start : '12:00',
+            end : '12:15'
+        },
+        icon : 'clock-o', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Перерва'}
+        ]
+    },
+    {
+        time : {
+            start : '12:15',
+            end : '12:55'
+        },
+        icon : 'file-text', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Доповідь 4'}
+        ]
+    },
+    {
+        time : {
+            start : '12:55',
+            end : '14:00'
+        },
+        icon : 'cutlery', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Обід'}
+        ]
+    },
+    {
+        time : {
+            start : '14:00',
+            end : '14:40'
+        },
+        icon : 'file-text', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Доповідь 5'}
+        ]
+    },
+    {
+        time : {
+            start : '14:40',
+            end : '14:55'
+        },
+        icon : 'clock-o', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Перерва'}
+        ]
+    },
+    {
+        time : {
+            start : '14:55',
+            end : '15:35'
+        },
+        icon : 'file-text', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Доповідь 6'}
+        ]
+    },
+    {
+        time : {
+            start : '15:35',
+            end : '15:50'
+        },
+        icon : 'clock-o', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Перерва'}
+        ]
+    },
+    {
+        time : {
+            start : '15:50',
+            end : '16:30'
+        },
+        icon : 'file-text', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Доповідь 7'}
+        ]
+    },
+    {
+        time : {
+            start : '16:30',
+            end : '16:45'
+        },
+        icon : 'clock-o', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Перерва'}
+        ]
+    },
+    {
+        time : {
+            start : '16:45',
+            end : '17:25'
+        },
+        icon : 'file-text', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Доповідь 8'}
+        ]
+    },
+    {
+        time : {
+            start : '17:25',
+            end : '17:40'
+        },
+        icon : 'clock-o', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Перерва'}
+        ]
+    },
+    {
+        time : {
+            start : '17:40',
+            end : '18:20'
+        },
+        icon : 'file-text', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Доповідь 9'}
+        ]
+    },
+    {
+        time : {
+            start : '18:20',
+            end : '18:35'
+        },
+        icon : 'clock-o', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Перерва'}
+        ]
+    },
+    {
+        time : {
+            start : '18:35',
+            end : '19:15'
+        },
+        icon : 'file-text', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Доповідь 10'}
+        ]
+    },
+    {
+        time : {
+            start : '19:15',
+            end : '19:30'
+        },
+        icon : 'flag', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'Закриття конференції'}
+        ]
+    },
+    {
+        time : {
+            start : '19:30',
+            end : '22:00'
+        },
+        icon : 'glass', // can be: 'user', 'file-text', 'clock-o', 'coffee', 'cutlery', 'flag', 'glass'
+        scheduleRendered : '',
+        talks : [
+            {author : '', title : 'After-party'}
+        ]
+    }
 ];
