c4ea4b01402f685bd347ab4d4f2de28ddb04fa52
wayfaerer <bodomhead@gmail.com>
2020-02-06 11:13:07 +0200

    speakers

--- resources/lang/js/ua/main-speakers-2020-ua.js
+++ resources/lang/js/ua/main-speakers-2020-ua.js
@@ -60,6 +60,67 @@ export const MainSpeakers2020Ua = [
                 fatype : 'linkedin'
             }
         ]
-    }
+    },
+    {
+        image : '/public/img/cont/speakers/amit_kumar.jpg',
+        name : 'Amit Kumar',
+        position : 'Software Engineer',
+        company : 'Aktech Labs',
+        place : 'London, UK',
+        rept : [
+            {
+                title : 'Building Pipelines with Airflow! [En]',
+                description :
+                    '<p>Airflow is an open-source data pipeline orchestration framework ' +
+                    'that allows developers to programmatically author, schedule and monitor data pipelines. ' +
+                    'In this talk we will go through Airflow\'s core concepts for workflow creation with some examples.</p>'
+            }
+        ],
+        aboutSpeaker : 'Amit is a presenter and a Software Engineer based in London, UK. He is an avid supporter of Open Source Software.',
+        socialsRendered : '',
+        socials : [
+            {
+                link : 'http://twitter.com/iaktech',
+                fatype : 'twitter'
+            },
+            {
+                link : 'http://github.com/iaktech',
+                fatype : 'github'
+            },
+            {
+                link : 'http://iamit.in',
+                fatype : 'link'
+            },
 
+        ]
+    },
+    {
+        image : '/public/img/cont/speakers/emily_woods.jpg',
+        name : 'Emily Woods',
+        position : '',
+        company : '',
+        place : 'Berlin, Germany',
+        rept : [
+            {
+                title : 'Taking care of the environment(s) and coding your infrastructure [En]',
+                description :
+                    '<p>Infrastructure as Code uses code to provision, configure, and manage infrastructure. ' +
+                    'With the right set of tools, it’s relatively straightforward to create a description ' +
+                    'of the infrastructure on which your applications should be deployed. In this talk, ' +
+                    'I’ll take a high-level look at the current Infrastructure as Code (IaC) landscape. ' +
+                    'I’ll discuss some of the challenges and benefits that IaC can bring - from version controlling ' +
+                    'your infrastructure, to creating consistent, resilient processes, and sustaining developer happiness. ' +
+                    'Lastly, I’ll look at some of the neat features which some IaC tools can offer you.</p>'
+            }
+        ],
+        aboutSpeaker : '',
+        socialsRendered : '',
+        socials : [
+            {
+                link : 'https://emilywoods.me',
+                fatype : 'link'
+            },
+
+        ]
+    }
 ];
