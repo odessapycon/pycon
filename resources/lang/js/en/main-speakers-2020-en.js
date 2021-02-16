a2a18f3244a62a910e4f991ef2deeb185e6ab83d
Vadim Linnikov <bodomhead@gmail.com>
2021-02-16 21:17:36 +0200

    speaker change

--- resources/lang/js/en/main-speakers-2020-en.js
+++ resources/lang/js/en/main-speakers-2020-en.js
@@ -212,17 +212,10 @@ export const MainSpeakers2020En = [
         image : '/public/img/cont/speakers/yehor_levchenko.jpg',
         name : 'Yehor Levchenko',
         position : 'Python Team Lead',
-        company : 'Serpstat',
         place : 'Odesa, Ukraine',
         rept : [
             {
-                title : 'Flexible parsing: how we parse everything and expect nothing [Ru]',
-                description :
-                    '<p>I will talk about difficulties we were facing during search engine results parsing and what we ' +
-                    'did to avoid them in future. During my speech I am going to share the concept of our new parsing ' +
-                    'logic, all it\'s iterations and changes, historical mistakes that made our legacy code weak and ' +
-                    'error prone, and surely technical details: third party libraries, work flow and how we support our ' +
-                    'new codebase.</p>'
+                title : 'To be announced'
             }
         ],
         aboutSpeaker : 'Working in marketing and development since 2010<br>' +
