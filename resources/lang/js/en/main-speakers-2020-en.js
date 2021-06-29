f29a755965431e1dde1d9c47beb0560ffd1793ba
Vadim Linnikov <bodomhead@gmail.com>
2021-06-29 19:05:05 +0300

    removed speaker

--- resources/lang/js/en/main-speakers-2020-en.js
+++ resources/lang/js/en/main-speakers-2020-en.js
@@ -32,48 +32,48 @@ export const MainSpeakers2020En = [
             }
         ]
     },
-    {
-        image : '/public/img/cont/speakers/emanuil_tolev.jpg',
-        name : 'Emanuil Tolev',
-        position : 'Community Engineer',
-        company : 'Elastic',
-        place : 'London, UK',
-        rept : [
-            {
-                title : 'The gentle touch of APM - how code tracing works in Python [En]',
-                description : '<p>It has been a busy several years in monitoring and observability. ' +
-                    'As we’ve hit limits on the visibility and detail that logging and metrics provide, ' +
-                    'we’ve turned to tracing and APM (App Performance Monitoring) systems. ' +
-                    'We can now understand performance bottlenecks and see errors in our apps down to the line of code. ' +
-                    'But how do they really work under the hood? Come and find out! ' +
-                    'We\'ll walk through how a free APM system works - Elastic APM.</p>' +
-                    '<p>- an overview of typical Python web framework structure<br>' +
-                    '- Elastic APM\'s tech architecture<br>' +
-                    '- how its Python agent hooks deep into web apps and Celery back-ends<br>' +
-                    '- how Python trace data is processed and visualised<br>' +
-                    '- how it avoids hitting production web app performance while tracing</p>' +
-                    '<p>This talk is friendly to a variety of backgrounds and levels of experience.' +
-                    ' It would help a lot if you have worked on a production Python web app,' +
-                    ' but the focus is on giving an introduction to how these things work.</p>'
-            }
-        ],
-        aboutSpeaker : '',
-        socialsRendered : '',
-        socials : [
-            {
-                link : 'https://twitter.com/emanuil_tolev/',
-                fatype : 'twitter'
-            },
-            {
-                link : 'https://github.com/emanuil-tolev/',
-                fatype : 'github'
-            },
-            {
-                link : 'https://www.linkedin.com/in/emanuil-tolev/',
-                fatype : 'linkedin'
-            }
-        ]
-    },
+    // {
+    //     image : '/public/img/cont/speakers/emanuil_tolev.jpg',
+    //     name : 'Emanuil Tolev',
+    //     position : 'Community Engineer',
+    //     company : 'Elastic',
+    //     place : 'London, UK',
+    //     rept : [
+    //         {
+    //             title : 'The gentle touch of APM - how code tracing works in Python [En]',
+    //             description : '<p>It has been a busy several years in monitoring and observability. ' +
+    //                 'As we’ve hit limits on the visibility and detail that logging and metrics provide, ' +
+    //                 'we’ve turned to tracing and APM (App Performance Monitoring) systems. ' +
+    //                 'We can now understand performance bottlenecks and see errors in our apps down to the line of code. ' +
+    //                 'But how do they really work under the hood? Come and find out! ' +
+    //                 'We\'ll walk through how a free APM system works - Elastic APM.</p>' +
+    //                 '<p>- an overview of typical Python web framework structure<br>' +
+    //                 '- Elastic APM\'s tech architecture<br>' +
+    //                 '- how its Python agent hooks deep into web apps and Celery back-ends<br>' +
+    //                 '- how Python trace data is processed and visualised<br>' +
+    //                 '- how it avoids hitting production web app performance while tracing</p>' +
+    //                 '<p>This talk is friendly to a variety of backgrounds and levels of experience.' +
+    //                 ' It would help a lot if you have worked on a production Python web app,' +
+    //                 ' but the focus is on giving an introduction to how these things work.</p>'
+    //         }
+    //     ],
+    //     aboutSpeaker : '',
+    //     socialsRendered : '',
+    //     socials : [
+    //         {
+    //             link : 'https://twitter.com/emanuil_tolev/',
+    //             fatype : 'twitter'
+    //         },
+    //         {
+    //             link : 'https://github.com/emanuil-tolev/',
+    //             fatype : 'github'
+    //         },
+    //         {
+    //             link : 'https://www.linkedin.com/in/emanuil-tolev/',
+    //             fatype : 'linkedin'
+    //         }
+    //     ]
+    // },
     {
         image : '/public/img/cont/speakers/amit_kumar.jpg',
         name : 'Amit Kumar',
